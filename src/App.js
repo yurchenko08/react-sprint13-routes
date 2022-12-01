581198cdfd56c5a1ac46860244fb0d0d67554fbc
yurchenko08 <yurchenko08@gmail.com>
2022-12-01 21:32:28 +0200

    fixed bugs subtask1

--- src/App.js
+++ src/App.js
@@ -6,22 +6,9 @@ import { Subtask1 } from './components/Subtask1';
 
 export default function App() {
   return (
-    <>
-      <div className='App'>
-        <h1>React Marathon</h1>
-        <h2>The topic 'Routes'</h2>
-        <a href='/'>Show protected information if</a>
-        <span>&nbsp;</span>
-        <input size='5'></input> is odd
-        <div className='mainClass'>
-          Go to the component programmatically, by checking the box:{' '}
-          <input type='checkbox'></input>
-        </div>
-      </div>
-      <Routes>
-        <Route path='/subtask1' element={<Subtask1 />} />
-        <Route path='/subtask1/:param' element={<Subtask1Param />} />
-      </Routes>
-    </>
+    <Routes>
+      <Route path='/subtask1' element={<Subtask1 />} />
+      <Route path='/subtask1/:param' element={<Subtask1Param />} />
+    </Routes>
   );
 }
--- src/components/Subtask1Param.js
+++ src/components/Subtask1Param.js
@@ -1,6 +1,18 @@
 import { useParams } from 'react-router-dom';
 export function Subtask1Param() {
   const { param } = useParams();
-  console.log(param);
-  return <div>Subtask1, parameter:{param}</div>;
+  return (
+    <div className='App'>
+      <h1>React Marathon</h1>
+      <h2>The topic 'Routes'</h2>
+      <a href='/'>Show protected information if</a>
+      <span>&nbsp;</span>
+      <input size='5'></input> is odd
+      <div className='mainClass'>
+        Go to the component programmatically, by checking the box:{' '}
+        <input type='checkbox'></input>
+      </div>
+      <div>Subtask1, parameter: {param}</div>
+    </div>
+  );
 }
