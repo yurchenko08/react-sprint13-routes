259e3116134f69c83c7515d2488722e6a8eecb02
yurchenko08 <yurchenko08@gmail.com>
2022-12-01 13:02:55 +0200

    subtask1

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React from 'react';
-
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import { Subtask1 } from './components/Subtask1';
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>React Marathon</h1>
-      <h2>The topic 'Routes'</h2>
-      <a href='/'>Show protected information if</a>
-      <span>&nbsp;</span>
-      <input size="5"></input> is odd
-      <div className="mainClass">
-        Go to the component programmatically, by checking the box:{" "}
-        <input type="checkbox"></input>
+    <>
+      <div className='App'>
+        <h1>React Marathon</h1>
+        <h2>The topic 'Routes'</h2>
+        <a href='/'>Show protected information if</a>
+        <span>&nbsp;</span>
+        <input size='5'></input> is odd
+        <div className='mainClass'>
+          Go to the component programmatically, by checking the box:{' '}
+          <input type='checkbox'></input>
+        </div>
       </div>
-    </div>
+      <Routes>
+        <Route path='subtask1/:param' element={<Subtask1 />} />
+      </Routes>
+    </>
   );
 }
