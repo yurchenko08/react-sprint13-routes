329ede4b2b12c58b6b1aabe6f55eea46bf203612
yurchenko08 <yurchenko08@gmail.com>
2022-12-02 19:11:36 +0200

    fixed bugs

--- src/App.js
+++ src/App.js
@@ -1,37 +1,55 @@
-import React, { useState } from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route, Link, Outlet, useNavigate } from 'react-router-dom';
+import Subtask1 from './components/Subtask1/Subtask1';
+import Subtask2 from './components/Subtask2/Subtask2';
+import ParamNum from './components/Subtask1/ParamNum';
+import Subtask3 from './components/Subtask3/Subtask3';
+import NotFound from './components/NotFound/NotFound';
 import './App.css';
-import { Subtask1Param } from './components/Subtask1Param';
-import { Subtask1 } from './components/Subtask1';
-import { Subtask2 } from './components/Subtask2';
-import { Subtask3 } from './components/Subtask3';
+import { useState } from 'react';
+import Subtask4 from './components/Subtask4/Subtask4';
 
 export default function App() {
-  const [checkDigit, setCheckdigit] = useState(false);
-  const [link, setLink] = useState('');
-  const handleInput = (e) => {
-    if (e.target.value % 2 === 1) {
-      setCheckdigit(true);
-      setLink('/subtask3');
-    } else setCheckdigit(false);
+  //subtask3
+  const [inputValue, setInputValue] = useState('');
+  const checkInputValue =
+    inputValue % 2 === 0 && inputValue !== 'undefined' ? false : true;
+  const link = checkInputValue ? 'subtask3' : '/';
+  const guard = checkInputValue ? <Subtask3 /> : <Outlet />;
+
+  //subtask4
+  const navigate = useNavigate();
+  const handleCheckboxChange = (e) => {
+    if (e.target.checked) {
+      navigate('/subtask4');
+    } else {
+      navigate('');
+    }
   };
+
   return (
-    <div className='App'>
+    <div className="App">
       <h1>React Marathon</h1>
       <h2>The topic 'Routes'</h2>
       <Link to={link}>Show protected information if</Link>
       <span>&nbsp;</span>
-      <input onChange={handleInput} size='5'></input> is odd
-      <div className='mainClass'>
+      <input
+        size="5"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      ></input>{' '}
+      is odd
+      <div className="mainClass">
         Go to the component programmatically, by checking the box:{' '}
-        <input type='checkbox'></input>
+        <input type="checkbox" onChange={handleCheckboxChange}></input>
       </div>
       <Routes>
-        <Route path='/subtask1' element={<Subtask1 />}>
-          <Route path=':param' element={<Subtask1 />} />
+        <Route path="/subtask1" element={<Subtask1 />}>
+          <Route path=":paramValue" element={<ParamNum />} />
         </Route>
-        <Route path='/subtask2' element={<Subtask2 />} />
-        {checkDigit && <Route path={link} element={<Subtask3 />}></Route>}
+        <Route path="subtask2" element={<Subtask2 />} />
+        <Route path={link} element={guard} />
+        <Route path="subtask4" element={<Subtask4 />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
   );
--- src/components/Subtask1Param.js
+++
@@ -1,5 +0,0 @@
-import { useParams } from 'react-router-dom';
-export function Subtask1Param() {
-  const { param } = useParams();
-  return <div>{param}</div>;
-}
