a27a068e7649924905a6ab78439f83aa0f2f3173
yurchenko08 <yurchenko08@gmail.com>
2022-12-02 15:49:06 +0200

    subtask3

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import { Subtask1Param } from './components/Subtask1Param';
 import { Subtask1 } from './components/Subtask1';
 import { Subtask2 } from './components/Subtask2';
+import { Subtask3 } from './components/Subtask3';
 
 export default function App() {
+  const digit = { odd: '/subtask3', even: '/' };
+  const [value, setValue] = useState('');
+  console.log(value);
   return (
     <div className='App'>
       <h1>React Marathon</h1>
       <h2>The topic 'Routes'</h2>
-      <a href='/'>Show protected information if</a>
+      <a href={+value % 2 !== 0 ? digit.odd : digit.even}>
+        Show protected information if
+      </a>
       <span>&nbsp;</span>
-      <input size='5'></input> is odd
+      <input
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        size='5'
+      ></input>{' '}
+      is odd
       <div className='mainClass'>
         Go to the component programmatically, by checking the box:{' '}
         <input type='checkbox'></input>
       </div>
       <Routes>
-        <Route path='/subtask1' element={<Subtask1 />} />
+        <Route path='subtask1' element={<Subtask1 />} />
         <Route path='/subtask1/:param' element={<Subtask1Param />} />
         <Route path='/subtask2' element={<Subtask2 />} />
+        <Route path='/subtask3' element={<Subtask3 />} />
       </Routes>
     </div>
   );
